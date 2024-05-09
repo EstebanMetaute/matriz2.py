3269e913aec51371d1fa3e7812e541c8830b5a76
Esteban Metaute <158522173+EstebanMetaute@users.noreply.github.com>
2024-05-09 17:58:22 -0500

    matriz 2
    matriz preguntas y respuestas

---
+++ matriz.py
@@ -0,0 +1,101 @@
+
+matriz = [['_', '_', '_', '_', '_'], ['_', '_', '_', '_', '_'], ['_', '_', '_', '_', '_'], ['_', '_', '_', '_', '_'], ['_', '_', '_', '_', '_' ]]
+
+buena = '❂'
+mala = '☢'
+import os
+def fnt_limpiar():
+    os.system('cls')
+    print('\n\n')
+    fnt_impresion()
+    print('\n\n')
+
+def fnt_agregar(x, y):
+    if x == 0 and y == 0:
+        if matriz [x][y] == '_':
+            matriz [x][y] = buena.upper()
+    elif x == 0 and y == 1:
+        if matriz [x][y] == '_':
+            matriz [x][y] = mala.upper()
+    elif x == 0 and y == 2:
+        if matriz [x][y] == '_':
+            matriz [x][y] = buena.upper()
+    elif x == 0 and y == 3:
+        if matriz [x][y] == '_':
+            matriz [x][y] = buena.upper()
+    elif x == 0 and y == 4:
+        if matriz [x][y] == '_':
+            matriz [x][y] = buena.upper()
+    elif x == 1 and y == 0:
+        if matriz [x][y] == '_':
+            matriz [x][y] = mala.upper()
+    elif x == 1 and y == 1:
+        if matriz [x][y] == '_':
+            matriz [x][y] = buena.upper()
+    elif x == 1 and y == 2:
+        if matriz [x][y] == '_':
+            matriz [x][y] = buena.upper()
+    elif x == 1 and y == 3:
+        if matriz [x][y] == '_':
+            matriz [x][y] = mala.upper()
+    elif x == 1 and y == 4:
+        if matriz [x][y] == '_':
+            matriz [x][y] = mala.upper()
+    elif x == 2 and y == 0:
+        if matriz [x][y] == '_':
+            matriz [x][y] = buena.upper()
+    elif x == 2 and y == 1:
+        if matriz [x][y] == '_':
+            matriz [x][y] = buena.upper()
+    elif x == 2 and y == 2:
+        if matriz [x][y] == '_':
+            matriz [x][y] = buena.upper()
+    elif x == 2 and y == 3:
+        if matriz [x][y] == '_':
+            matriz [x][y] = mala.upper()
+    elif x == 2 and y == 4:
+        if matriz [x][y] == '_':
+            matriz [x][y] = buena.upper()
+    elif x == 3 and y == 0:
+        if matriz [x][y] == '_':
+            matriz [x][y] = buena.upper()
+    elif x == 3 and y == 1:
+        if matriz [x][y] == '_':
+            matriz [x][y] = mala.upper()
+    elif x == 3 and y == 2:
+        if matriz [x][y] == '_':
+            matriz [x][y] = mala.upper()
+    elif x == 3 and y == 3:
+        if matriz [x][y] == '_':
+            matriz [x][y] = buena.upper()
+    elif x == 3 and y == 4:
+        if matriz [x][y] == '_':
+            matriz [x][y] = buena.upper()
+    elif x == 4 and y == 0:
+        if matriz [x][y] == '_':
+            matriz [x][y] = buena.upper()
+    elif x == 4 and y == 1:
+        if matriz [x][y] == '_':
+            matriz [x][y] = mala.upper()
+    elif x == 4 and y == 2:
+        if matriz [x][y] == '_':
+            matriz [x][y] = buena.upper()
+    elif x == 4 and y == 3:
+        if matriz [x][y] == '_':
+            matriz [x][y] = buena.upper()
+    elif x == 4 and y == 4:
+        if matriz [x][y] == '_':
+            matriz [x][y] = mala.upper()
+    
+def fnt_impresion():
+    for i in range(len(matriz)):
+        for j in range(len(matriz[i])):
+            print(matriz[i][j], end=' ')
+        print()
+
+sw = True
+while sw == True:
+    import os
+    os.system('cls')
+    fnt_impresion()
+    fnt_agregar(int(input('fila: ')), int(input('columna: ')))
